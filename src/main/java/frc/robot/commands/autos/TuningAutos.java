9aad16a06621dec61b9892714d90f21f0b8aa859
Your Name <you@example.com>
2024-02-16 11:51:10 -0600

    PathPlanner Fixed?

---
+++ src/main/java/frc/robot/commands/autos/TuningAutos.java
@@ -0,0 +1,79 @@
+package frc.robot.commands.autos;
+
+import frc.robot.commands.drive.PathPlannerFollow;
+import frc.robot.subsystems.drive.SwerveDrive;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public final class TuningAutos {
+    public static Command forwardThenTurnTest(SwerveDrive drive) {
+        return PathPlannerFollow.create(drive, "ForwardThenTurnTest")
+            .setMaxVelocity(1)
+            .setAcceleration(1)
+            .addMarker("test", new InstantCommand())//ToDo:Test
+            .build();
+    }
+    public static Command forwardTest(SwerveDrive drive) {//Top Red/Bottom Blue
+        return new SequentialCommandGroup(
+            PathPlannerFollow.create(drive, "ForwardTest")
+                .setMaxVelocity(1)
+                .setAcceleration(3)
+                .build()
+        );
+    }
+    public static Command backTest(SwerveDrive drive) {//Top Red/Bottom Blue
+        return new SequentialCommandGroup(
+            PathPlannerFollow.create(drive, "BackwardTest")
+                .setMaxVelocity(1)
+                .setAcceleration(3)
+                .build()
+        );
+    }
+    public static Command turnTest(SwerveDrive drive) {
+        return new SequentialCommandGroup(
+            PathPlannerFollow.create(drive, "TurnTest")
+                .setMaxVelocity(0.2)
+                .setAcceleration(0.2)
+                .build(),
+            drive.setOffsetCommand(drive.getRotation2d().rotateBy(Rotation2d.fromDegrees(180)).getDegrees())//Works
+        );
+
+    }
+    public static Command splineTest(SwerveDrive drive) {
+        return new SequentialCommandGroup(
+            PathPlannerFollow.create(drive, "SplineTest")
+                .setMaxVelocity(1)
+                .setAcceleration(1)
+                .build()
+        );
+    }
+    public static Command lineToLinearTest(SwerveDrive drive) {
+        return new SequentialCommandGroup(
+            PathPlannerFollow.create(drive, "LineToTest")
+                .setMaxVelocity(0.2)
+                .setAcceleration(0.2)
+                .build()
+        );
+    }
+    public static Command strafeRight(SwerveDrive drive) {
+        return new SequentialCommandGroup(
+            PathPlannerFollow.create(drive, "StrafeRightTest")
+                .setMaxVelocity(0.2)
+                .setAcceleration(0.2)
+                .build()
+        );
+    }
+    public static Command strafeLeft(SwerveDrive drive) {
+        return new SequentialCommandGroup(
+            PathPlannerFollow.create(drive, "StrafeLeftTest")
+                .setMaxVelocity(0.2)
+                .setAcceleration(0.2)
+                .build()
+        );
+    }
+
+
+    private TuningAutos() {}
+}
--- src/main/java/frc/robot/commands/drive/PathPlannerFollow.java
+++ src/main/java/frc/robot/commands/drive/PathPlannerFollow.java
@@ -1,21 +1,18 @@
 package frc.robot.commands.drive;
 
 import java.util.HashMap;
-import java.util.List;
 
 import com.pathplanner.lib.auto.AutoBuilder;
-import com.pathplanner.lib.commands.PathPlannerAuto;
-import com.pathplanner.lib.controllers.PathFollowingController;
-import com.pathplanner.lib.path.PathConstraints;
+import com.pathplanner.lib.auto.NamedCommands;
 import com.pathplanner.lib.path.PathPlannerPath;
 import com.pathplanner.lib.path.PathPlannerTrajectory;
 import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
 import com.pathplanner.lib.util.PIDConstants;
-import com.pathplanner.lib.util.PathPlannerLogging;
 import com.pathplanner.lib.util.ReplanningConfig;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.subsystems.drive.SwerveDrive;
 import frc.robot.subsystems.drive.SwerveDriveConstants;
 
@@ -25,6 +22,7 @@ public class PathPlannerFollow {
     private double maxVelocity = 0.3;
     private double acceleration = 0.5;
     private HashMap<String, Command> eventMap = new HashMap<>();
+
 
     private PathPlannerFollow(SwerveDrive drive, String pathName, double maxVelocity, double acceleration, HashMap<String, Command> eventMap) {
         this.drive = drive;
@@ -55,91 +53,49 @@ public class PathPlannerFollow {
         eventMap.put(name, toRun);
         return new PathPlannerFollow(drive, pathName, maxVelocity, acceleration, eventMap);
     }
-    // public PathPlannerFollow addMarker(String name, ParallelCommandGroup toRun) {
-    //     eventMap.put(name, toRun);
-    //     return new PathPlannerFollow(drive, pathName, maxVelocity, acceleration, eventMap);
+    public PathPlannerFollow addMarker(String name, ParallelCommandGroup toRun) {
+        eventMap.put(name, toRun);
+        return new PathPlannerFollow(drive, pathName, maxVelocity, acceleration, eventMap);
+    }
+    public Command build(){
+        // PathPlannerPath path = new PathPlannerPath(drive, pathName)
+        // PathPlannerTrajectory pathGroup = new PathPlannerTrajectory(pathName);
+        AutoBuilder.configureHolonomic(
+            drive::getPose, // Robot pose supplier
+                drive::resetOdometry, // Method to reset odometry (will be called if your auto has a starting pose)
+                drive::getSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+                drive::drive, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
+                //TODO:Test WILL THIS WORK? IS IT ROBOT RELATIVE OR FIELD CENTRIC
+                new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
+                    new PIDConstants(SwerveDriveConstants.SwerveDriveConfig.TRANSLATIONAL_KP.get(), 
+                        SwerveDriveConstants.SwerveDriveConfig.TRANSLATIONAL_KI.get(), 
+                        SwerveDriveConstants.SwerveDriveConfig.TRANSLATIONAL_KD.get()),  // Translation PID constants
+                    new PIDConstants(SwerveDriveConstants.SwerveDriveConfig.THETA_KP.get(), 
+                        SwerveDriveConstants.SwerveDriveConfig.THETA_KI.get(), 
+                        SwerveDriveConstants.SwerveDriveConfig.THETA_KD.get()),  // Rotation PID constants
+                    4.5, // Max module speed, in m/s
+                    0.4, // Drive base radius in meters. Distance from robot center to furthest module.
+                    new ReplanningConfig() // Default path replanning config. See the API for the options here; Ba
+                ),
+                () -> {
+                    // Boolean supplier that controls when the path will be mirrored for the red alliance
+                    // This will flip the path being followed to the red side of the field.
+                    // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+                    var alliance = DriverStation.getAlliance();
+                    if (alliance.isPresent()) {
+                        return alliance.get() == DriverStation.Alliance.Red;
+                    }
+                    return false;
+                },
+                drive // Reference to this subsystem to set requirements\
+            );
+        return AutoBuilder.buildAuto(pathName);
+
+    }
+    // public Command getAuto(String name){
+    //     return AutoBuilder.buildAuto(name);
     // }
-
-    // public Command build3() {
-    //     List<PathPlannerTrajectory> pathGroup = PathPlanner.loadPathGroup(
-    //         pathName, 
-    //         new PathConstraints(maxVelocity, acceleration)
-    //     );
-        
-
-    //     // Create the AutoBuilder. This only needs to be created once when robot code starts, 
-    //     // not every time you want to create an auto command. A good place to put this is in 
-    //     // RobotContainer along with your subsystems.
-    //     AutoBuilder.
-    //     SwerveAutoBuilder autoBuilder = new SwerveAutoBuilder(
-    //         drive::getPose, // Pose2d supplier
-    //         drive::resetOdometry, // Pose2d consumer, used to reset odometry at the beginning of auto
-    //         SwerveDrive.DRIVE_KINEMATICS, // SwerveDriveKinematics
-    //         new PIDConstants(SwerveDriveConstants.SwerveDriveConfig.TRANSLATIONAL_KP.get(), 
-    //             SwerveDriveConstants.SwerveDriveConfig.TRANSLATIONAL_KI.get(), 
-    //             SwerveDriveConstants.SwerveDriveConfig.TRANSLATIONAL_KD.get()), 
-    //             // PID constants to correct for translation error (used to create the X and Y PID controllers)
-    //         new PIDConstants(SwerveDriveConstants.SwerveDriveConfig.THETA_KP.get(), 
-    //             SwerveDriveConstants.SwerveDriveConfig.THETA_KI.get(), 
-    //             SwerveDriveConstants.SwerveDriveConfig.THETA_KD.get()), 
-    //             // PID constants to correct for rotation error (used to create the rotation controller)
-    //         drive::setFeedforwardModuleStates, // Module states consumer used to output to the drive subsystem
-    //         eventMap,
-    //         true, // Should the path be automatically mirrored depending on alliance color. Optional, defaults to true
-    //         drive // The drive subsystem. Used to properly set the requirements of path following commands
-    //     );
-        
-    //     //TODO check if there are any other better constructors ^^
-
-    //     return autoBuilder.fullAuto(pathGroup);
-    // }
-    
-//     public Command build(){
-//         List<PathPlannerTrajectory> pathGroup = PathPlanner.loadPathGroup(
-//             pathName, 
-//             new PathConstraints(maxVelocity, Math.pow(maxVelocity, 2), acceleration, Math.pow(acceleration,2))
-//         );
-// PathConstraints;
-// PathFollowingController;
-// PathPlannerLogging;
-// PathPlannerAuto;
-// PathPlannerPath;
-//         PathPlannerTrajectory.State.;
-//         AutoBuilder autoBuilder = new AutoBuilder();
-//         // autoBuilder =
-//         AutoBuilder.configureHolonomic(
-//                 drive::getPose, // Robot pose supplier
-//                 drive::resetOdometry, // Method to reset odometry (will be called if your auto has a starting pose)
-//                 drive::getSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
-//                 drive::drive, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
-//                 //TODO:Test WILL THIS WORK? IS IT ROBOT RELATIVE OR FIELD CENTRIC
-//                 new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
-//                     new PIDConstants(SwerveDriveConstants.SwerveDriveConfig.TRANSLATIONAL_KP.get(), 
-//                         SwerveDriveConstants.SwerveDriveConfig.TRANSLATIONAL_KI.get(), 
-//                         SwerveDriveConstants.SwerveDriveConfig.TRANSLATIONAL_KD.get()),  // Translation PID constants
-//                     new PIDConstants(SwerveDriveConstants.SwerveDriveConfig.THETA_KP.get(), 
-//                         SwerveDriveConstants.SwerveDriveConfig.THETA_KI.get(), 
-//                         SwerveDriveConstants.SwerveDriveConfig.THETA_KD.get()),  // Rotation PID constants
-//                     4.5, // Max module speed, in m/s
-//                     0.4, // Drive base radius in meters. Distance from robot center to furthest module.
-//                     new ReplanningConfig() // Default path replanning config. See the API for the options here; Ba
-//                 ),
-//                 () -> {
-//                     // Boolean supplier that controls when the path will be mirrored for the red alliance
-//                     // This will flip the path being followed to the red side of the field.
-//                     // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
-//                     var alliance = DriverStation.getAlliance();
-//                     if (alliance.isPresent()) {
-//                         return alliance.get() == DriverStation.Alliance.Red;
-//                     }
-//                     return false;
-//                 },
-//                 drive // Reference to this subsystem to set requirements
-//         );
-//         return autoBuilder.followPath(pathGroup)
-//     }
     //Translation PID constants - PID constants to correct for translation error (used to create the X and Y PID controllers)
     //Rotation PID constants - PID constants to correct for rotation error (used to create the rotation controller)
-
     
 }
