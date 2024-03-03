7d6f60b8e5fe63e6bca2f9ed501dc1ab7861ffd3
Your Name <you@example.com>
2024-03-02 18:00:21 -0600

    ShooterFeedforward

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -11,18 +11,19 @@ import frc.robot.utility.shuffleboard.ShuffleboardValue;
 public class Shooter extends SubsystemBase {
 public class Constants {
     // (out)4/3 (input)
-        public static final double GEAR_RATIO = 1 / 180;//Old One is 240 // New is 180 (I think)
+        public static final double GEAR_RATIO = 3/4;//Old One is 240 // New is 180 (I think)
         public static final double READINGS_PER_REVOLUTION = 1;
         public static final double ROTATIONS_TO_RADIANS = (GEAR_RATIO * READINGS_PER_REVOLUTION) / (Math.PI * 2);
+        public static final double CONVERSION = 4/3;
     }
 
     public enum ShooterSpeeds {
         //15000+
-        AMP_SHOOT(200),
-        SPEAKER_SHOOT(10000),
+        AMP_SHOOT(5000),//200
+        SPEAKER_SHOOT(12000),//10000
         HOLD(SPEAKER_SHOOT.get()*.3),
         STOP(0), 
-        CLAW_TRANSFER(4000),
+        CLAW_TRANSFER(4000),//4000
         POSITION_TOLERANCE(5),
 
         ;
@@ -59,8 +60,8 @@ public class Constants {
             19,
             false,
             IdleMode.Coast,
-            1,
-            1,
+            Constants.ROTATIONS_TO_RADIANS,
+            Constants.ROTATIONS_TO_RADIANS,
             this.isEnabled,
                 ShuffleboardValue.create(0.0, "VoltageL", Shooter.class.getSimpleName())
                     .build()
@@ -69,8 +70,8 @@ public class Constants {
             18,
             false,
             IdleMode.Coast,
-            1,
-            1,
+            Constants.ROTATIONS_TO_RADIANS,
+            Constants.ROTATIONS_TO_RADIANS,
             this.isEnabled,
                 ShuffleboardValue.create(0.0, "VoltageR", Shooter.class.getSimpleName())
                     .build() 
@@ -80,15 +81,22 @@ public class Constants {
             0.00005,//0.000173611    //.00005
             0,
             0);
-        feedforward = new SimpleMotorFeedforward(0.000,0,0);
+        // feedforward = new SimpleMotorFeedforward(0.000,0,0);
+        feedforward = new SimpleMotorFeedforward(.3246, 2.1305, .58723);//from sysid
+        // feedforward = new SimpleMotorFeedforward(.01, 0.01305, 0);
+
         // feedforward = new SimpleMotorFeedforward(5.24, 5.7, 1);
         shooterController.setTolerance(ShooterSpeeds.POSITION_TOLERANCE.get());
     }
 
     @Override
     public void periodic() {
+        encoderVelocityWriter.set(getVelocity());
+        encoderVelocityErrorWriter.set(shooterController.getVelocityError());
         setPower(shooterController.calculate(getVelocity())+
             feedforward.calculate(getVelocity(), getVelocity()));
+
+        
     }
       
     @Override
