90a4bd6e951b0b2d15b10ba98a116e76befea7cf
Your Name <you@example.com>
2024-03-03 01:21:34 -0600

    Intake Sensors

--- src/main/java/frc/robot/subsystems/intake/ColorSensor.java
+++ src/main/java/frc/robot/subsystems/intake/ColorSensor.java
@@ -10,11 +10,6 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 @Deprecated
 public class ColorSensor extends SubsystemBase {
-    public static class DropDownConstants {
-        public static final double GEAR_RATIO = 1 / 180;//Old One is 240 // New is 180 (I think)
-        public static final double READINGS_PER_REVOLUTION = 1;
-        public static final double ROTATIONS_TO_RADIANS = (GEAR_RATIO * READINGS_PER_REVOLUTION) / (Math.PI * 2);
-    }
     //TODO: Check this
     protected final ColorSensorV3 colorSensor;
     protected final ColorMatch colorMatcher;
@@ -35,12 +30,14 @@ public class ColorSensor extends SubsystemBase {
         updateColorSensor();
         
     }
+
     //Color Sensor Functions
     private void updateColorSensor(){
         colorMatcher.addColorMatch(orange);
         detectedColor = colorSensor.getColor(); 
         match = colorMatcher.matchClosestColor(detectedColor);
     }
+    
     public boolean isElementIn(){
         return match.color == orange;
         // return encoderVelocityErrorWriter.get()<-2000;
---
+++ src/main/java/frc/robot/subsystems/intake/LimitSwitch.java
@@ -0,0 +1,28 @@
+package frc.robot.subsystems.intake;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LimitSwitch extends SubsystemBase {
+    protected final DigitalInput intakeLimitSwitch; //Limit Switch?
+
+    public LimitSwitch(Boolean isEnabled) {
+        intakeLimitSwitch = new DigitalInput(5);//WHERE is it plugged in
+    }
+
+    @Override
+    public void periodic() {
+        // if(intakeLimitSwitch.get()){    //NEED to check whether to add !
+        //     resetEncoder();
+        // }
+    }
+  
+    @Override
+    public void simulationPeriodic() {
+        periodic();
+    }
+
+    public boolean isLimitSwitch(){
+        return intakeLimitSwitch.get();
+    }
+}  
