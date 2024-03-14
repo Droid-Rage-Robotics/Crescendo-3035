02c4cca83225094ffdbf67f807fa68c2dcfe0244
Your Name <you@example.com>
2024-03-14 02:10:31 -0500

    Lights

--- src/main/java/frc/robot/subsystems/Light.java
+++ src/main/java/frc/robot/subsystems/Light.java
@@ -1,20 +1,28 @@
 package frc.robot.subsystems;
 
 
+import com.revrobotics.ColorSensorV3.LEDCurrent;
+
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.utility.shuffleboard.ShuffleboardValue;
 
 public class Light extends SubsystemBase {
     private final AddressableLED ledOne;
     // private final AddressableLED ledTwo;
     private final AddressableLEDBuffer bufferOne;
+    
+    
     // private final AddressableLEDBuffer bufferTwo;
 
+    private final ShuffleboardValue<String> lightWriter = ShuffleboardValue.create
+        ("Color", "Light Color", Light.class.getSimpleName())
+        .build();
     
-    private int LED_COUNT_ONE = 26;   //Number of LEDS on Strip (Can have multiple strips)
+    private int LED_COUNT_ONE = 25;   //Number of LEDS on Strip (Can have multiple strips)
     // private int LED_COUNT_TWO = 39;   //Number of LEDS on Strip (Can have multiple strips)
     public final Color red = Color.kRed, 
                       batteryBlue = Color.kMidnightBlue,
@@ -24,7 +32,8 @@ public class Light extends SubsystemBase {
                       blue = Color.kBlue, //Decoration
                       green = Color.kGreen, //Ready to Shoot?
                       white = Color.kWhite; //Aligned?
-    class Rainbow{
+    
+                      class Rainbow{
       private int rainbowFirstPixelHue = 0;
     }
     class SwitchLED{
@@ -41,7 +50,7 @@ public class Light extends SubsystemBase {
     private Rainbow rainbow;
     
     public Light() {
-        ledOne = new AddressableLED(1);
+        ledOne = new AddressableLED(2);
         // ledTwo = new AddressableLED(1);
         bufferOne = new AddressableLEDBuffer(LED_COUNT_ONE);
         // bufferTwo = new AddressableLEDBuffer(LED_COUNT_TWO);
@@ -49,10 +58,12 @@ public class Light extends SubsystemBase {
         ledOne.setLength(bufferOne.getLength());
         ledOne.setData(bufferOne);
         ledOne.start();
+        flashingColor = new FlashingColor();
     }
 
     @Override
     public void periodic() {
+      // lightWriter.set(bufferOne.getLED(2).toString());
       ledOne.setData(bufferOne);
       // ledTwo.setData(bufferTwo);
     }
@@ -62,32 +73,32 @@ public class Light extends SubsystemBase {
         periodic();
     }
   
-    public void rainbow() {
-        // For every pixel
-        for (int i = 0; i < bufferOne.getLength(); i++) {
-          // Calculate the hue - hue is easier for rainbows because the color
-          // shape is a circle so only one value needs to precess
-          final var hue = (rainbow.rainbowFirstPixelHue + (i * 180 / bufferOne.getLength())) % 180;
-          // Set the value
-          bufferOne.setHSV(i, hue, 255, 128);
-        }
-        // Increase by to make the rainbow "move"
-        rainbow.rainbowFirstPixelHue += 3;
-        // Check bounds
-        rainbow.rainbowFirstPixelHue %= 180;
+    // public void rainbow() {
+    //     // For every pixel
+    //     for (int i = 0; i < bufferOne.getLength(); i++) {
+    //       // Calculate the hue - hue is easier for rainbows because the color
+    //       // shape is a circle so only one value needs to precess
+    //       final var hue = (rainbow.rainbowFirstPixelHue + (i * 180 / bufferOne.getLength())) % 180;
+    //       // Set the value
+    //       bufferOne.setHSV(i, hue, 255, 128);
+    //     }
+    //     // Increase by to make the rainbow "move"
+    //     rainbow.rainbowFirstPixelHue += 3;
+    //     // Check bounds
+    //     rainbow.rainbowFirstPixelHue %= 180;
 
-        // for (int i = 0; i < bufferTwo.getLength(); i++) {
-        //   // Calculate the hue - hue is easier for rainbows because the color
-        //   // shape is a circle so only one value needs to precess
-        //   final var hue = (rainbow.rainbowFirstPixelHue + (i * 180 / bufferTwo.getLength())) % 180;
-        //   // Set the value
-        //   bufferTwo.setHSV(i, hue, 255, 128);
-        // }
-        // // Increase by to make the rainbow "move"
-        // rainbow.rainbowFirstPixelHue += 3;
-        // // Check bounds
-        // rainbow.rainbowFirstPixelHue %= 180;
-    }
+    //     // for (int i = 0; i < bufferTwo.getLength(); i++) {
+    //     //   // Calculate the hue - hue is easier for rainbows because the color
+    //     //   // shape is a circle so only one value needs to precess
+    //     //   final var hue = (rainbow.rainbowFirstPixelHue + (i * 180 / bufferTwo.getLength())) % 180;
+    //     //   // Set the value
+    //     //   bufferTwo.setHSV(i, hue, 255, 128);
+    //     // }
+    //     // // Increase by to make the rainbow "move"
+    //     // rainbow.rainbowFirstPixelHue += 3;
+    //     // // Check bounds
+    //     // rainbow.rainbowFirstPixelHue %= 180;
+    // }
 
     // private void rainbow2() {
     //   List<Color> colors = List.of(
@@ -124,6 +135,8 @@ public class Light extends SubsystemBase {
   }
 
     public void setAllColor(Color color) {
+      lightWriter.set(color.toString());
+
       for (int i = 0; i < bufferOne.getLength(); i++) {
         bufferOne.setLED(i, color);
       }
