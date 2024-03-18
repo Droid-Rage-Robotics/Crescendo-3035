7a68465e3f0380585b99bf695c6cc0b0093fabf4
Your Name <you@example.com>
2024-03-17 23:09:11 -0500

    Fixed Cycle Tracker

--- src/main/java/frc/robot/commands/TeleopShoot.java
+++ src/main/java/frc/robot/commands/TeleopShoot.java
@@ -1,18 +1,21 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Shooter.ShooterSpeeds;
 import frc.robot.subsystems.intake.Intake;
+import frc.robot.utility.InfoTracker.CycleTracker;
 
 public class TeleopShoot extends SequentialCommandGroup {
-    public TeleopShoot (Intake intake, Shooter shooter){
+    public TeleopShoot (Intake intake, Shooter shooter, CycleTracker cycleTracker){
         addCommands(
-            new SequentialCommandGroup(
+            new ParallelCommandGroup(
                 shooter.runOnce(()->shooter.setTargetVelocity(ShooterSpeeds.SPEAKER_SHOOT)),
-                intake.setPositionCommand(Intake.Value.SHOOTER_TRANSFER)
+                intake.setPositionCommand(Intake.Value.SHOOTER_TRANSFER),
+                new InstantCommand(()-> cycleTracker.trackCycle(ShooterSpeeds.SPEAKER_SHOOT))
                 )
             );
     }   
