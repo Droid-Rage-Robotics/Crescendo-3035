479d4e5b6638ab26c7e9c8fb9c15ed8cf5570026
Your Name <you@example.com>
2024-03-18 16:21:54 -0500

    Added the Position Shuffle Board on Absolute Encoder

--- src/main/java/frc/robot/subsystems/intake/dropDown/IntakeDropDownAbsolute.java
+++ src/main/java/frc/robot/subsystems/intake/dropDown/IntakeDropDownAbsolute.java
@@ -14,7 +14,7 @@ public class IntakeDropDownAbsolute extends IntakeDropDown {
     private SafeSparkAbsoluteEncoder absoluteEncoder;
     public IntakeDropDownAbsolute(Boolean isEnabled, SafeCanSparkMax sparkMax) {
         super(isEnabled);
-        absoluteEncoder = new SafeSparkAbsoluteEncoder(sparkMax,false, (Math.PI * 2), (Math.PI * 2 / 60));
+        absoluteEncoder = new SafeSparkAbsoluteEncoder(sparkMax,false, (Math.PI * 2), (Math.PI * 2 / 60), this);
         // absoluteEncoder = sparkMax.getAbsoluteEncoder(SparkAbsoluteEncoder.Type.kDutyCycle);
         
         // absoluteEncoder.setPositionConversionFactor(Math.PI * 2);
--- src/main/java/frc/robot/utility/motor/SafeSparkAbsoluteEncoder.java
+++ src/main/java/frc/robot/utility/motor/SafeSparkAbsoluteEncoder.java
@@ -3,16 +3,41 @@ package frc.robot.utility.motor;
 import com.revrobotics.SparkAbsoluteEncoder;
 import com.revrobotics.SparkAbsoluteEncoder.Type;
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.intake.Intake;
+import frc.robot.utility.shuffleboard.ShuffleboardValue;
+
 public class SafeSparkAbsoluteEncoder {
     private SparkAbsoluteEncoder encoder;
+    private final SubsystemBase base;
+    protected final ShuffleboardValue<Double> degreeWriter;
+    protected final ShuffleboardValue<Double> radianWriter;
+    protected final ShuffleboardValue<Double> rawWriter;
+
+
 
     //Math.PI*2 = 360 Degrees
     //Math.PI*2/60
-    public SafeSparkAbsoluteEncoder(SafeCanSparkMax motor, boolean isInverted, double positionConversionFactor, double velocityConversionFactor){
+    public SafeSparkAbsoluteEncoder(SafeCanSparkMax motor, boolean isInverted, 
+        double positionConversionFactor, double velocityConversionFactor, SubsystemBase base){
         encoder = motor.getAbsoluteEncoder(Type.kDutyCycle);
         encoder.setInverted(isInverted);
         encoder.setPositionConversionFactor(positionConversionFactor);
         encoder.setVelocityConversionFactor(velocityConversionFactor);
+        this.base = base;
+
+        rawWriter = ShuffleboardValue
+            .create(0.0, base.getName()+"/Pos/Raw", base.getClass().getSimpleName())
+            .withSize(1, 2)
+            .build();
+        degreeWriter = ShuffleboardValue
+            .create(0.0, base.getName()+"/Pos/Degree", base.getClass().getSimpleName())
+            .withSize(1, 2)
+            .build();
+        radianWriter = ShuffleboardValue
+            .create(0.0, base.getName()+"/Pos/Radian", base.getClass().getSimpleName())
+            .withSize(1, 2)
+            .build();
     } 
     public double getPosition() {
         return encoder.getPosition();
