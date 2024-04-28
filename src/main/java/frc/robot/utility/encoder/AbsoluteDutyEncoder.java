40b01045a30a12f499d008a3ea52f181d059d6f6
Your Name <you@example.com>
2024-04-27 19:31:42 -0500

    Renamed Writers

--- src/main/java/frc/robot/utility/encoder/AbsoluteDutyEncoder.java
+++ src/main/java/frc/robot/utility/encoder/AbsoluteDutyEncoder.java
@@ -7,43 +7,44 @@ import frc.robot.utility.shuffleboard.ShuffleboardValue;
 // @Deprecated
 public class AbsoluteDutyEncoder extends SubsystemBase {
     protected final DutyCycleEncoder encoder;
-    protected final ShuffleboardValue<Double> encoderPos = ShuffleboardValue.create
+    protected final ShuffleboardValue<Double> encoderPosWriter = ShuffleboardValue.create
         (0.0, "Encoder Position", AbsoluteDutyEncoder.class.getSimpleName())
         .build();
     protected final ShuffleboardValue<Double> encoderPosFlip = ShuffleboardValue.create
-        (0.0, "Encoder Position Flip", AbsoluteDutyEncoder.class.getSimpleName())
+        (0.0, "Encoder Position FlipTemporary", AbsoluteDutyEncoder.class.getSimpleName())
         .build();
-    protected final ShuffleboardValue<Boolean> isConnectedValue = ShuffleboardValue.create
-        (true, "isConnectedValue", AbsoluteDutyEncoder.class.getSimpleName())
+    protected final ShuffleboardValue<Boolean> isConnectedWriter = ShuffleboardValue.create
+        (true, "isConnected", AbsoluteDutyEncoder.class.getSimpleName())
         .build();
-    protected final ShuffleboardValue<Double> offset = ShuffleboardValue.create
+    protected final ShuffleboardValue<Double> offsetWriter = ShuffleboardValue.create
         (0.0, "Offset", AbsoluteDutyEncoder.class.getSimpleName())
         .build();
-        protected final ShuffleboardValue<Double> distance = ShuffleboardValue.create
+        protected final ShuffleboardValue<Double> distanceWriter = ShuffleboardValue.create
         (0.0, "Distance", AbsoluteDutyEncoder.class.getSimpleName())
         .build();
     private boolean isInverted;
-    public AbsoluteDutyEncoder(int channelNum, double distancePerRotation, double offset) {
+    public AbsoluteDutyEncoder(int channelNum, boolean isInverted, double distancePerRotation, double offset) {
         encoder = new DutyCycleEncoder(channelNum);
         encoder.setDistancePerRotation(distancePerRotation);//Can you do .5?
+        this.isInverted=isInverted;
 
-        encoder.setPositionOffset(0);
+        encoder.setPositionOffset(offset);
         // encoder.reset();
         // encoder.isConnected();//use this to show disconnections of encoder
     }
 
     @Override
     public void periodic() {
-        offset.set(encoder.getPositionOffset());
+        offsetWriter.set(encoder.getPositionOffset());
 
-        isConnectedValue.set(encoder.isConnected());
+        isConnectedWriter.set(encoder.isConnected());
         // encoderPos.set((encoder.getAbsolutePosition()-encoder.getPositionOffset())+.75);
-        encoderPos.set(calculateAbsPos());
+        encoderPosWriter.set(calculateAbsPos());
         encoderPosFlip.set(1-(encoder.getAbsolutePosition()-encoder.getPositionOffset()));
 
 
         // encoderPos.set(encoder.getDistance());
-        distance.set(encoder.getDistancePerRotation());
+        distanceWriter.set(encoder.getDistancePerRotation());
     }
   
     @Override
@@ -53,15 +54,14 @@ public class AbsoluteDutyEncoder extends SubsystemBase {
 
     public double calculateAbsPos(){
         double givenPos = encoder.getAbsolutePosition()-encoder.getPositionOffset();
-        if(givenPos<0){
+        if(givenPos<0){ //To account for negative values/rollovers
             givenPos=1-Math.abs(givenPos);
+            //givePose=1+givenPos
         }
 
-        if(isInverted){
-            return 1-givenPos;
-        }{
-            return givenPos;
+        if(isInverted){ //To invert values based on direction
+            givenPos = 1-givenPos;
         }
-
+        return givenPos*encoder.getDistancePerRotation();
     }
 }  
