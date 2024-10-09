0576fba0d831a2a687a0afa29cc0f8cf642d8d30
Sai Varshini Chinnasani <saivarshini.chinnasani@gmail.com>
2024-10-08 22:48:40 -0500

    Thing

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -21,6 +21,7 @@ import frc.robot.subsystems.ampMech.AmpMechIntake;
 import frc.robot.subsystems.ampMech.ampMechArm.AmpMechArmAbsolute;
 import frc.robot.subsystems.climb.Climb;
 import frc.robot.subsystems.climb.ClimbAlternate;
+import frc.robot.subsystems.drive.OwnSixWheel;
 import frc.robot.subsystems.drive.SwerveDrive;
 import frc.robot.subsystems.intake.Intake;
 import frc.robot.subsystems.intake.IntakeWheel;
@@ -44,18 +45,19 @@ import frc.robot.utility.template.Arm;
 //current stuff
 public class Robot extends TimedRobot {
     //15 missing^
-    private final SwerveDrive drive = new SwerveDrive(true);//2-10 Works
-    private final Shooter shooter = new Shooter(false);//18,19 Works
+    private final OwnSixWheel drive = new OwnSixWheel();
+    // private final SwerveDrive drive = new SwerveDrive(true);//2-10 Works
+    // private final Shooter shooter = new Shooter(false);//18,19 Works
 
-    private final AmpMechElevator elevator = new AmpMechElevator(false);//22-DO NOT TURN THIS ON
-    private final AmpMechIntake ampIntake = new AmpMechIntake(false);//24
-    private final AmpMechArmAbsolute arm = new AmpMechArmAbsolute(false, ampIntake.getMotor());//23
-    private final AmpMech ampMech = new AmpMech(elevator, arm, ampIntake);
+    // private final AmpMechElevator elevator = new AmpMechElevator(false);//22-DO NOT TURN THIS ON
+    // private final AmpMechIntake ampIntake = new AmpMechIntake(false);//24
+    // private final AmpMechArmAbsolute arm = new AmpMechArmAbsolute(false, ampIntake.getMotor());//23
+    // private final AmpMech ampMech = new AmpMech(elevator, arm, ampIntake);
 
-    private final ClimbAlternate climb = new ClimbAlternate(false,false);//20,21
-    private final IntakeWheel intakeWheel = new IntakeWheel(false);//16
-    private final IntakeDropDownAbsolute dropDown = new IntakeDropDownAbsolute(false, climb.getMotorR());//17
-    private final Intake intake = new Intake(dropDown, intakeWheel);
+    // private final ClimbAlternate climb = new ClimbAlternate(false,false);//20,21
+    // private final IntakeWheel intakeWheel = new IntakeWheel(false);//16
+    // private final IntakeDropDownAbsolute dropDown = new IntakeDropDownAbsolute(false, climb.getMotorR());//17
+    // private final Intake intake = new Intake(dropDown, intakeWheel);
     // private AbsoluteDutyEncoder encoder = 
     //     new AbsoluteDutyEncoder(0,false, 1,0.38, ampIntake);
     
@@ -160,8 +162,8 @@ public class Robot extends TimedRobot {
 		// drive.driveAutoReset();//TODO:Test
         // robotContainer.configureTeleOpBindings(drive, intake, shooter, ampMech, climb, cycleTracker,vision
         // );
-        robotContainer.testCommands(vision, drive);
-        // robotContainer.testSixWheel();
+        // robotContainer.testCommands(vision, drive);
+        robotContainer.testSixWheel(drive);
         // teleopButtons.newTeleopButtons( climb, intake, shooter, ampMech , drive);
     }
 
